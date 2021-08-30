22248dc56b604d402416c5d5f96d7fc1d6b05851
James Arbogast <jamesarbogast52@gmail.com>
2021-08-30 16:45:13 -0700

    udpated nav names

--- client/src/sharedComponents/Navbar.js
+++ client/src/sharedComponents/Navbar.js
@@ -17,38 +17,19 @@ function ShopNavbar() {
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        
-        <Nav.Link href="/items">Shop</Nav.Link>
-        <NavDropdown title="Clothing" id="basic-nav-dropdown"> 
+        <NavDropdown title="Shop" id="basic-nav-dropdown"> 
           
-          <NavDropdown.Item href="#action/3.1">Shirts</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Sweaters</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Knits</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Bottoms</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Suits & Accessories</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Outerwear</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Denim</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Lounge</NavDropdown.Item>
+          <NavDropdown title="New Arrivals">New Arrivals</NavDropdown>
+          <NavDropdown title="Clothing">Clothing</NavDropdown>
+          <NavDropdown title="About">About</NavDropdown>
+          <NavDropdown title="Blog">Blog</NavDropdown>
           {/* <NavDropdown.Divider /> */}
         </NavDropdown>
-        <NavDropdown title="Accessories" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Footwear</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Socks</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Sunglasses</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Hats</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Watches & Jewelry</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Wallets & Clips</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Bags & Briefcases</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Winter Accessories</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Belts</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Grooming & Cologne</NavDropdown.Item>
           {/* <NavDropdown.Divider /> */}
-        </NavDropdown>
-        <Nav.Link href="#link">Sale</Nav.Link>
+        <Nav.Link href="#link">Brands</Nav.Link>
+        <Nav.Link href="/about">About</Nav.Link>
         <Nav.Link href="/blogs">Blog</Nav.Link>
-        <Nav.Link href="/view/cart">Account</Nav.Link>
         <Nav.Link href="/view/cart">Cart</Nav.Link>
-        <Nav.Link href="#link">Gift Card</Nav.Link>
         
       </Nav>
     </Navbar.Collapse>
